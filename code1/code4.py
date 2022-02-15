4e7bac8ffeaade4e47216325a9f5a67a13e5a820
min1mi <min2mi00@gmail.com>
2022-02-15 16:28:31 +0900

    반올림 오류 수정

--- code1/code4.py
+++ code1/code4.py
@@ -2,7 +2,10 @@ import sys
 sys.stdin = open('code1/input.txt', 'rt')
 n = int(input())
 a_list = list(map(int, input().split()))
-avg = round(sum(a_list)/n)
+#round()는 round_half_even 방식을 택함
+# avg = round(sum(a_list)/n)
+avg = int(sum(a_list)/n + 0.5)
+
 min = float('inf')
 for i, x in enumerate(a_list):
     tmp = abs(x-avg)
